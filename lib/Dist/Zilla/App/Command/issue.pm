ae4fc40b2620d07c6809164cafc5fabaf6f8ee9b
Erik Carlsson <info@code301.com>
2016-01-13 14:06:10 +0000

    Init

---
+++ lib/Dist/Zilla/App/Command/issue.pm
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+
+package Dist::Zilla::App::Command::issue;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Dist::Zilla::App::Command::issue;
+
+=head1 DESCRIPTION
+
+Dist::Zilla::App::Command::issue is ...
+
+=head1 SEE ALSO
+
+=cut
